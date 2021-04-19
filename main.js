346bd11a75043308a20bda2df2ec635c268e490b
sbigelo <sean.bigelow1@gmail.com>
2021-04-19 14:29:25 -0400

    commit tree 8c7c34a10756710938a6d2b39913161368c46112

--- main.js
+++ main.js
@@ -1,8 +1,43 @@
 // Defining text characters for the empty and full hearts for you to use later.
 const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
-
+const modalClass = document.getElementById('modal')
+const heart = document.querySelectorAll('.like-glyph')
 // Your JavaScript code goes here!
+
+heart.forEach( h => {
+  h.addEventListener('click', userLiked)
+})
+
+function userLiked(event) {
+  let target = event.target
+  mimicServerCall("google.com")
+    .then(message => { 
+      target.classList.add('activated-heart')
+      target.textContent = FULL_HEART
+      target.removeEventListener('click', userLiked)
+      target.addEventListener('click', userUnliked)
+    })
+    .catch(error => {
+      modalClass.classList.remove("hidden")
+      const p = document.createElement('p')
+      const errorMessage = document.createTextNode(error)
+      p.appendChild(errorMessage)
+      modalClass.appendChild(p)
+      setTimeout(function() {modalClass.classList.add("hidden")}, 3000)
+})
+}
+
+function userUnliked(event) {
+  let target = event.target
+  target.classList.remove('activated-heart')
+  target.textContent = EMPTY_HEART
+  target.removeEventListener('click', userUnliked)
+  target.addEventListener('click', userLiked)
+}
+  
+
+
 
 
 
